f4abf09f0e9f651204ec556be4474fcb0e22a8a3
kumar-myself <myselffile@gmail.com>
2026-03-13 21:50:24 +0530

    Update data_loader.py

--- source/data_loader.py
+++ source/data_loader.py
@@ -1,1 +1,23 @@
-source
+import os
+import requests
+import base64
+import pandas as pd
+from io import StringIO
+
+
+def load_csv():
+    url = os.environ["DATA_URL"]
+    token = os.environ["TOKEN"]
+
+    headers = {
+        "Authorization": f"token {token}"
+    }
+
+    response = requests.get(url, headers=headers)
+    res = response.json()
+
+    content = base64.b64decode(res["content"]).decode("utf-8")
+
+    df = pd.read_csv(StringIO(content))
+
+    return df
