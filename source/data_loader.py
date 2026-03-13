b35cd77a3fbf5617da87310f597a0522d7cc03bb
kumar-myself <myselffile@gmail.com>
2026-03-13 22:39:09 +0530

    Update data_loader.py v2

--- source/data_loader.py
+++ source/data_loader.py
@@ -6,18 +6,35 @@ from io import StringIO
 
 
 def load_csv():
-    url = os.environ["SOURCE_DATA_URL"]
-    token = os.environ["TOKEN"]
+
+    url = os.environ.get("SOURCE_DATA_URL")
+    token = os.environ.get("TOKEN")
+
+    if not url:
+        raise ValueError("SOURCE_DATA_URL not found in environment variables")
+
+    if not token:
+        raise ValueError("TOKEN not found in environment variables")
 
     headers = {
         "Authorization": f"token {token}"
     }
 
     response = requests.get(url, headers=headers)
+
+    print("HTTP Status:", response.status_code)
+
     res = response.json()
+
+    # check GitHub API error
+    if "content" not in res:
+        raise Exception(f"GitHub API error: {res}")
 
     content = base64.b64decode(res["content"]).decode("utf-8")
 
     df = pd.read_csv(StringIO(content))
+
+    print("CSV loaded successfully")
+    print("Rows:", len(df))
 
     return df
