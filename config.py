33fe9148a0e8a0d35fd9b206d4631b259b3e412f
kumar-myself <myselffile@gmail.com>
2026-03-14 19:07:58 +0530

    Update config.py v5

--- config.py
+++ config.py
@@ -20,6 +20,7 @@ SOURCE_FILE = f"{SOURCE_DIR}{INPUT_CSV}"
 OUTPUT_DIR  = f"{BASE_DIR}output/"
 BATCH_DIR   = f"{OUTPUT_DIR}url_batches/"
 PROCESSED_BATCH_DIR = f"{OUTPUT_DIR}processed/"
+PROGRESS_DIR = "https://api.github.com/repos/kumar-myself/chain-forensic/contents/"
 
 TOKEN = os.environ.get("TOKEN")
 PROGRESS_FILE = f'progress.json'
