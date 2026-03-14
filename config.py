5983be0d59fb988ebf5c9c11cbdb3fac021eb708
kumar-myself <myselffile@gmail.com>
2026-03-14 15:14:32 +0530

    Update config.py v2

--- config.py
+++ config.py
@@ -1,12 +1,5 @@
 import os
 
-# Directories
-SOURCE_DATA_URL = os.environ.get("SOURCE_DATA_URL")
-OUTPUT_DATA_DIR = os.environ.get("OUTPUT_DATA_DIR")
-TOKEN = os.environ.get("TOKEN")
-BATCH_DIR = f'{OUTPUT_DATA_DIR}url_batches/'
-PROGRESS_FILE = f'progress.json'
-
 # Input
 INPUT_CSV = 'sitemap-1.csv'
 INPUT_CSV_COLUMN = 'loc'
@@ -16,3 +9,17 @@ INPUT_URL_FILTER = '/address/'
 BATCH_SIZE = 50
 MAX_CONCURRENT = 2
 MAX_RETRIES = 2
+
+
+# Directories
+BASE_DIR = os.environ.get("BASE_DIR")
+# Derived paths
+SOURCE_DIR  = f"{BASE_DIR}input/"
+SOURCE_FILE = f"{SOURCE_DIR}{INPUT_CSV}"
+
+OUTPUT_DIR  = f"{BASE_DIR}output/"
+BATCH_DIR   = f"{OUTPUT_DIR}url_batches/"
+
+TOKEN = os.environ.get("TOKEN")
+PROGRESS_FILE = f'progress.json'
+
